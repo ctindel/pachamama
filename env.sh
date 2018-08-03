aa8286bd40ad42cdcbd60e58211aba161354781e
Chad Tindel <chad@tindel.net>
2018-08-03 11:37:06 +0000

    completed

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PM_ENV=prod
-export GCLOUD_PROJECT=ctindel-142217
+export GCLOUD_PROJECT=ctindel-211916
 export PM_PROJECT=insurance
-#export PM_ORG_ID=924627718475
+export PM_ORG_ID=924627718475
